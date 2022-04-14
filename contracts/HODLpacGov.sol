1c9634d1a5fe65b3a1b5097cd61f83f1a051d6a4
Ethan Harsh <ethan@ethanharsh.com>
2022-04-14 12:53:22 -0400

    init

---
+++ contracts/HODLpacGov.sol
@@ -0,0 +1,100 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.4;
+
+import "@openzeppelin/contracts-upgradeable/token/ERC20/ERC20Upgradeable.sol";
+import "@openzeppelin/contracts-upgradeable/token/ERC20/extensions/ERC20BurnableUpgradeable.sol";
+import "@openzeppelin/contracts-upgradeable/security/PausableUpgradeable.sol";
+import "@openzeppelin/contracts-upgradeable/access/AccessControlUpgradeable.sol";
+import "@openzeppelin/contracts-upgradeable/token/ERC20/extensions/draft-ERC20PermitUpgradeable.sol";
+import "@openzeppelin/contracts-upgradeable/token/ERC20/extensions/ERC20VotesUpgradeable.sol";
+import "@openzeppelin/contracts-upgradeable/proxy/utils/Initializable.sol";
+import "@openzeppelin/contracts-upgradeable/proxy/utils/UUPSUpgradeable.sol";
+
+contract HODLpacGov is Initializable, ERC20Upgradeable, ERC20BurnableUpgradeable, PausableUpgradeable, AccessControlUpgradeable, ERC20PermitUpgradeable, ERC20VotesUpgradeable, UUPSUpgradeable {
+    bytes32 public constant PAUSER_ROLE = keccak256("PAUSER_ROLE");
+    bytes32 public constant MINTER_ROLE = keccak256("MINTER_ROLE");
+    bytes32 public constant UPGRADER_ROLE = keccak256("UPGRADER_ROLE");
+    bytes32 public constant TRANSFER_TOKEN_ROLE = keccak256("TRANSFER_TOKEN_ROLE");
+    bytes32 public constant TRANSFER_FROM_TOKEN_ROLE = keccak256("TRANSFER_FROM_TOKEN_ROLE");
+
+    /// @custom:oz-upgrades-unsafe-allow constructor
+    constructor() initializer {}
+
+    function initialize() initializer public {
+        __ERC20_init("HODLpacGov", "HODL");
+        __ERC20Burnable_init();
+        __Pausable_init();
+        __AccessControl_init();
+        __ERC20Permit_init("HODLpacGov");
+        __UUPSUpgradeable_init();
+
+        _grantRole(DEFAULT_ADMIN_ROLE, msg.sender);
+        _grantRole(PAUSER_ROLE, msg.sender);
+        _grantRole(MINTER_ROLE, msg.sender);
+        _grantRole(UPGRADER_ROLE, msg.sender);
+        _grantRole(TRANSFER_TOKEN_ROLE, msg.sender);
+        _grantRole(TRANSFER_FROM_TOKEN_ROLE, msg.sender);
+    }
+
+    function pause() public onlyRole(PAUSER_ROLE) {
+        _pause();
+    }
+
+    function unpause() public onlyRole(PAUSER_ROLE) {
+        _unpause();
+    }
+
+    function mint(address to, uint256 amount) public onlyRole(MINTER_ROLE) {
+        _mint(to, amount);
+    }
+
+    function transfer(address to, uint256 amount) public override onlyRole(TRANSFER_TOKEN_ROLE) returns(bool) {
+        address owner = _msgSender();
+        _transfer(owner, to, amount);
+        return true;
+    }
+
+    function transferFrom(address from, address to, uint256 amount) public override onlyRole(TRANSFER_FROM_TOKEN_ROLE) returns(bool) {
+        address spender = _msgSender();
+        _spendAllowance(from, spender, amount);
+        _transfer(from, to, amount);
+        return true;
+    }
+
+    function _beforeTokenTransfer(address from, address to, uint256 amount)
+        internal
+        whenNotPaused
+        override
+    {
+        super._beforeTokenTransfer(from, to, amount);
+    }
+
+    function _authorizeUpgrade(address newImplementation)
+        internal
+        onlyRole(UPGRADER_ROLE)
+        override
+    {}
+
+    // The following functions are overrides required by Solidity.
+
+    function _afterTokenTransfer(address from, address to, uint256 amount)
+        internal
+        override(ERC20Upgradeable, ERC20VotesUpgradeable)
+    {
+        super._afterTokenTransfer(from, to, amount);
+    }
+
+    function _mint(address to, uint256 amount)
+        internal
+        override(ERC20Upgradeable, ERC20VotesUpgradeable)
+    {
+        super._mint(to, amount);
+    }
+
+    function _burn(address account, uint256 amount)
+        internal
+        override(ERC20Upgradeable, ERC20VotesUpgradeable)
+    {
+        super._burn(account, amount);
+    }
+}
---
+++ test/hodlpac-governance-test.js
@@ -0,0 +1,205 @@
+const { expect } = require("chai");
+const { ethers, upgrades } = require("hardhat");
+const { expectRevert, BN } = require("@openzeppelin/test-helpers");
+
+describe("HODLPacGovernance Token Contract", function () {
+  let HODLPacGovernance;
+  let token;
+  let owner;
+  let addr1;
+  let addr2;
+  let addrs;
+
+  beforeEach(async function () {
+    HODLPacGovernance = await ethers.getContractFactory("HODLPacGovernance");
+    [owner, addr1, addr2, ...addrs] = await ethers.getSigners();
+
+    token = await upgrades.deployProxy(HODLPacGovernance, {
+      kind: "uups",
+    });
+  });
+
+  describe("Deployment", function () {
+    it("Should assign the total supply of tokens to the owner.", async function () {
+      const ownerBalance = await token.balanceOf(owner.address);
+      expect(await token.totalSupply()).to.equal(ownerBalance);
+    });
+  });
+
+  describe("Mint", function () {
+    it("Should mint 100 tokens to the owner.", async function () {
+      await token.mint(owner.address, 100);
+      const ownerBalance = await token.balanceOf(owner.address);
+      expect(ownerBalance).to.equal(100);
+    });
+
+    it("Should mint 100 tokens to addr1.", async function () {
+      await token.mint(addr1.address, 100);
+      const addr1Balance = await token.balanceOf(addr1.address);
+      expect(addr1Balance).to.equal(100);
+    });
+
+    it("Should not allow addr1 to mint 100 tokens to addr1.", async function () {
+      // await token.connect(addr1).mint(addr1.address, 100);
+      await expectRevert.unspecified(
+        token.connect(addr1).mint(addr1.address, 100)
+      );
+    });
+
+    it("Should not allow addr2 to mint 100 tokens to addr1.", async function () {
+      // await token.connect(addr1).mint(addr1.address, 100);
+      await expectRevert.unspecified(
+        token.connect(addr2).mint(addr1.address, 100)
+      );
+    });
+  });
+
+  describe("Transactions", function () {
+    it("Should transfer tokens between owner and accounts.", async function () {
+      await token.mint(owner.address, 100);
+      await token.transfer(addr1.address, 50);
+      const addr1Balance = await token.balanceOf(addr1.address);
+      let ownerBalance = await token.balanceOf(owner.address);
+      expect(addr1Balance).to.equal(50);
+      expect(ownerBalance).to.equal(50);
+      await token.transfer(addr2.address, 25);
+      const addr2Balance = await token.balanceOf(addr2.address);
+      ownerBalance = await token.balanceOf(owner.address);
+      expect(addr2Balance).to.equal(25);
+      expect(ownerBalance).to.equal(25);
+    });
+    it("Should not transfer tokens between non-owner accounts.", async function () {
+      await token.mint(owner.address, 100);
+      await token.transfer(addr1.address, 50);
+      let addr1Balance = await token.balanceOf(addr1.address);
+      let ownerBalance = await token.balanceOf(owner.address);
+      expect(addr1Balance).to.equal(50);
+      expect(ownerBalance).to.equal(50);
+      await token.transfer(addr2.address, 25);
+      let addr2Balance = await token.balanceOf(addr2.address);
+      ownerBalance = await token.balanceOf(owner.address);
+      expect(addr2Balance).to.equal(25);
+      expect(ownerBalance).to.equal(25);
+      expectRevert.unspecified(
+        token.connect(addr1).transfer(addr2.address, 25)
+      );
+      expectRevert.unspecified(
+        token.connect(addr1).transfer(owner.address, 25)
+      );
+      expectRevert.unspecified(
+        token.connect(addr2).transfer(addr1.address, 25)
+      );
+      addr1Balance = await token.balanceOf(addr1.address);
+      addr2Balance = await token.balanceOf(addr2.address);
+      ownerBalance = await token.balanceOf(owner.address);
+      expect(addr1Balance).to.equal(50);
+      expect(addr2Balance).to.equal(25);
+      expect(ownerBalance).to.equal(25);
+    });
+
+    it("Should not allow owner to transfer more tokens than it has.", async function () {
+      expectRevert(
+        token.transfer(addr1.address, 250),
+        "ERC20: transfer amount exceeds balance"
+      );
+      let ownerBalance = await token.balanceOf(owner.address);
+      expect(ownerBalance).to.equal(0);
+
+      await token.mint(owner.address, 100);
+      expectRevert(
+        token.transfer(addr1.address, 250),
+        "ERC20: transfer amount exceeds balance"
+      );
+      ownerBalance = await token.balanceOf(owner.address);
+      expect(ownerBalance).to.equal(100);
+    });
+
+    it("Should allow owner to transfer from one account to another.", async function () {
+      await token.mint(owner.address, 100);
+      await token.transfer(addr1.address, 50);
+      await token.connect(addr1).approve(owner.address, 25);
+      await token.transferFrom(addr1.address, addr2.address, 25);
+      const ownerBalance = await token.balanceOf(owner.address);
+      const addr1Balance = await token.balanceOf(addr1.address);
+      const addr2Balance = await token.balanceOf(addr2.address);
+      expect(ownerBalance).to.equal(50);
+      expect(addr1Balance).to.equal(25);
+      expect(addr2Balance).to.equal(25);
+    });
+
+    it("Should not allow other accounts to transfer from one account to another.", async function () {
+      await token.mint(owner.address, 100);
+      await token.transfer(addr1.address, 50);
+      await token.connect(addr1).approve(addr2.address, 25);
+      expectRevert.unspecified(
+        token.connect(addr2).transferFrom(addr1.address, owner.address, 25)
+      );
+      const ownerBalance = await token.balanceOf(owner.address);
+      const addr1Balance = await token.balanceOf(addr1.address);
+      const addr2Balance = await token.balanceOf(addr2.address);
+      expect(ownerBalance).to.equal(50);
+      expect(addr1Balance).to.equal(50);
+      expect(addr2Balance).to.equal(0);
+    });
+
+    it("Should not allow owner account to transfer more tokens than an account currently owns.", async function () {
+      await token.connect(addr1).approve(owner.address, 25);
+      await expectRevert.unspecified(
+        token.transferFrom(addr1.address, addr2.address, 25)
+      );
+      await token.mint(owner.address, 100);
+      await token.transfer(addr1.address, 1);
+      await expectRevert.unspecified(
+        token.transferFrom(addr1.address, addr2.address, 25)
+      );
+    });
+  });
+  describe("Pause", function () {
+    it("Should pause transactions.", async function () {
+      await token.mint(owner.address, 100);
+      await token.pause();
+      await expectRevert.unspecified(token.transfer(addr1.address, 50));
+      const ownerBalance = await token.balanceOf(owner.address);
+      const addr1Balance = await token.balanceOf(addr1.address);
+      const addr2Balance = await token.balanceOf(addr2.address);
+      expect(ownerBalance).to.equal(100);
+      expect(addr1Balance).to.equal(0);
+      expect(addr2Balance).to.equal(0);
+    });
+
+    it("Should unpause transactions.", async function () {
+      await token.mint(owner.address, 100);
+      await token.pause();
+      await expectRevert.unspecified(token.transfer(addr1.address, 50));
+      await token.unpause();
+      await token.transfer(addr1.address, 50);
+      const ownerBalance = await token.balanceOf(owner.address);
+      const addr1Balance = await token.balanceOf(addr1.address);
+      const addr2Balance = await token.balanceOf(addr2.address);
+      expect(ownerBalance).to.equal(50);
+      expect(addr1Balance).to.equal(50);
+      expect(addr2Balance).to.equal(0);
+    });
+  });
+
+  describe("Burn", function () {
+    it("Should burn specified number of tokens.", async function () {
+      await token.mint(owner.address, 100);
+      await token.burn(50);
+      const ownerBalance = await token.balanceOf(owner.address);
+      expect(ownerBalance).to.equal(50);
+    });
+    it("Owner should burn specified number of tokens in acct1.", async function () {
+      await token.mint(owner.address, 100);
+      await token.transfer(addr1.address, 50);
+      await token.connect(addr1).approve(owner.address, 500);
+      await token.burnFrom(addr1.address, 25);
+      const ownerBalance = await token.balanceOf(owner.address);
+      const addr1Balance = await token.balanceOf(addr1.address);
+      const addr2Balance = await token.balanceOf(addr2.address);
+      expect(ownerBalance).to.equal(50);
+      expect(addr1Balance).to.equal(25);
+      expect(addr2Balance).to.equal(0);
+    });
+  });
+});
